fd8a054730017f69d2cbab5835579aff49f3dcdc
becia1995 <33333720+becia1995@users.noreply.github.com>
2022-05-19 14:28:36 +0200

    Add files via upload

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# polybar mybar &
+polybar example -r &
+
+if [[ $(xrandr -q | grep 'DP1-3 connected') ]]; then
+	polybar second &
+fi
+
+if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
+	polybar third &
+fi
+
+echo "Polybar launched..."
